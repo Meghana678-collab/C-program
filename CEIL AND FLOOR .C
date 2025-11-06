0acaadb56cbfa92b1d995a3c8936292817ea2f6f
Meghana678-collab <mmeghanabj@gmail.com>
2025-11-06 22:38:20 +0530

    Create CEIL AND FLOOR .C

---
+++ CEIL AND FLOOR .C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("roundof  of 3.14 is %f \n" , ceil(3.14));
+     printf("roundof  of 18.0008 is %f \n" , floor(18.0008));
+    printf("roundof  of 18.0008 is %f \n" , ceil(18.0008));
+    return 0;
+}
